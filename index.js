91d04e121591973fd4c42d7389a0968d07504d5e
aaaziim <azim210215@gmail.com>
2025-01-24 15:18:01 +0600

    Single Service and Booking API Created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -30,11 +30,25 @@ async function run() {
 
 
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
 
 
     app.get("/services", async(req, res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
+        res.send(result)
+    })
+
+
+    app.get("/services/:id", async(req, res)=>{
+        const result = await serviceCollection.findOne({_id: new ObjectId(req.params.id)});
+        res.send(result)
+    })
+
+
+    app.post("/services", async(req, res)=>{
+        const doc = req.body;
+        const result = await bookingCollection.insertOne(doc)
         res.send(result)
     })
 
